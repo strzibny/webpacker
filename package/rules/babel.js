0fb75067a954cdc78612bc2be865247cd63c6574
Josef Strzibny <strzibny@strzibny.name>
2018-06-07 16:59:01 +0200

    Do not exclude modules

--- package/rules/babel.js
+++ package/rules/babel.js
@@ -3,7 +3,6 @@ const { cache_path: cachePath } = require('../config')
 
 module.exports = {
   test: /\.(js|jsx)?(\.erb)?$/,
-  exclude: /node_modules/,
   use: [
     {
       loader: 'babel-loader',
